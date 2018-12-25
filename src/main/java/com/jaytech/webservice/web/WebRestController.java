f7ecd2b4daae8bb27025a8222d5f61609d047a8f
pjh374968@gmail.com <pjh374968@gmail.com>
2018-12-25 19:35:30 +0900

    Create Posts model extending BaseTimeEntity, create its controller, dto

--- src/main/java/com/jaytech/webservice/web/WebRestController.java
+++ src/main/java/com/jaytech/webservice/web/WebRestController.java
@@ -1,13 +1,26 @@
 package com.jaytech.webservice.web;
 
+import com.jaytech.webservice.domain.posts.PostsRepository;
+import com.jaytech.webservice.dto.posts.PostsSaveRequestDto;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class WebRestController {
+
+    private PostsRepository postsRepository;
 
     @GetMapping("/hello")
     public String hello() {
         return "Hello World";
+    }
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto) {
+        postsRepository.save(dto.toEntity());
     }
 }
